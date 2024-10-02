3ebb2b482c61a40d2677c7529b3a3e87444980c8
JeronimoLedesma <ledesmajero@gmail.com>
2024-10-02 20:06:15 -0300

    Arreglos
    hecho en clase para probar si se podia buildear

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float jumpForce;
@@ -21,7 +21,7 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canJump==true)
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             canJump = false;
